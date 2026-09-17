3254674f7e458f9cbe7f0298f3102e37b1de6ba7
Acelbyte <marcelloemar@gmail.com>
2026-09-17 11:43:41 +0700

    Update website

--- api/admin/[...path].js
+++ api/admin/[...path].js
@@ -6,11 +6,16 @@ import { sendError } from '../_lib/http.js';
 export default async function handler(req, res) {
   const { path } = req.query; // path is an array: e.g. ['allowlist'], ['pending'], ['users', '123']
 
-  if (!path || !path.length) {
+  let pathArray = [];
+  if (path) {
+    pathArray = Array.isArray(path) ? path : path.split('/');
+  }
+
+  if (!pathArray.length) {
     return sendError(res, 404, 'Admin route not found');
   }
 
-  const route = path[0];
+  const route = pathArray[0];
 
   switch (route) {
     case 'allowlist':
@@ -19,8 +24,8 @@ export default async function handler(req, res) {
       return await pendingHandler(req, res);
     case 'users':
       // The users handler expects the ID as part of the req.query, or we can just inject it
-      if (path.length > 1) {
-        req.query.id = path[1];
+      if (pathArray.length > 1) {
+        req.query.id = pathArray[1];
         return await usersHandler(req, res);
       }
       return sendError(res, 404, 'User ID missing');
